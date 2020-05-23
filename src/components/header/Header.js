c48bd4f7cdee1cd40ec768cbaddf43e919f21891
rudyt7 <gallacticsoccer@gmail.com>
2020-05-23 12:56:27 +0530

    Barebones added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './Header.css';
 
 const Header = (props) => {
-	return <header className="header">Header</header>;
+	return (
+		<header className="header">
+			<h1>TO DO APP</h1>
+			<div>SignUp/ Sign In</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,7 +2,56 @@ import React from 'react';
 import './Nav.css';
 
 const Nav = (props) => {
-	return <nav className="nav">Side Nav</nav>;
+	return (
+		<nav className="nav">
+			<div>
+				<p>New</p>
+				<ul>
+					<li>
+						<a href="#pablo">Number of to do's</a>
+					</li>
+				</ul>
+			</div>
+			<br />
+
+			<div>
+				<p>In Progress</p>
+				<ul>
+					<li>
+						<a href="#pablo">Number of to do's</a>
+					</li>
+				</ul>
+			</div>
+			<br />
+			<div>
+				<p>Completed</p>
+				<ul>
+					<li>
+						<a href="#pablo">Number of to do's</a>
+					</li>
+				</ul>
+			</div>
+			<br />
+			<div>
+				<p>Labels</p>
+				<ul>
+					<li>
+						<a href="#pablo">Personal</a>
+					</li>
+					<li>
+						<a href="#pablo">Work</a>
+					</li>
+
+					<li>
+						<a href="#pablo">Shopping</a>
+					</li>
+					<li>
+						<a href="#pablo">Others</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
