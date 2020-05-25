b8f5e89941bc3d5dde480b9b339e5364b8585720
Aditya Saroj <adityasaroj001@gmail.com>
2020-05-25 23:47:30 +0530

    added ongoing button

--- src/components/main/ListItem.js
+++ src/components/main/ListItem.js
@@ -1,40 +1,65 @@
-import React from 'react';
-import './ListItem.css';
+import React from "react";
+import "./ListItem.css";
 
 const ListItem = (props) => {
-	function isEmptyOrSpaces(str) {
-		return str === null || str.match(/^ *$/) !== null;
-	}
+  function isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null;
+  }
 
-	// let listItem = null;
+  // let listItem = null;
 
-	if (isEmptyOrSpaces(props.title)) {
-		return false;
-	} else {
-		// switch (props.type) {
-		//     case 'personal':
-		//       break;
-		//     case 'work':
-		//       break;
-		//     case 'shopping':
-		//       break;
-		//     case 'others':
-		//       break;
-		//     default:
-		//       break;
-		// }
-	}
+  if (isEmptyOrSpaces(props.title)) {
+    return false;
+  } else {
+    // switch (props.type) {
+    //     case 'personal':
+    //       break;
+    //     case 'work':
+    //       break;
+    //     case 'shopping':
+    //       break;
+    //     case 'others':
+    //       break;
+    //     default:
+    //       break;
+    // }
+  }
 
-	return (
-		<li className="list-item" id="list-item">
-			<div style={{ float: 'right' }}>
-				<button className="itembtn comp">&#10003;</button>
-				<button className="itembtn del">&#10005;</button>
-			</div>
-			<h2>{props.title}</h2>
-			{props.children}
-		</li>
-	);
+  function handleOngoing() {}
+
+  function handleComplete() {}
+
+  function handleDelete() {}
+
+  return (
+    <li className="list-item" id="list-item">
+      <div style={{ float: "right" }}>
+        <button
+          className="itembtn ongoing"
+          data-hover="Ongoing"
+          onClick={() => handleOngoing()}
+        >
+          O
+        </button>
+        <button
+          className="itembtn comp"
+          data-hover="Complete"
+          onClick={() => handleComplete()}
+        >
+          &#10003;
+        </button>
+        <button
+          className="itembtn del"
+          data-hover="Delete"
+          onClick={() => handleDelete()}
+        >
+          &#10005;
+        </button>
+      </div>
+      <h1>{props.title}</h1>
+      {props.children}
+    </li>
+  );
 };
 
 export default ListItem;
