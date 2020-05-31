fe334f036ae7b87d213d5fffab72a63fa7dacb7c
rudyt7 <gallacticsoccer@gmail.com>
2020-05-31 15:16:59 +0530

    Updates

---
+++ precache-manifest.2ce220cd875086a18cfd6aa8e5a889cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2edfd1e42a613ad01033af476cf8bb5",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "9c46db483dd737b95af8",
+    "url": "/to-do-list/static/css/main.a1cc99b3.chunk.css"
+  },
+  {
+    "revision": "742e27e75b40a6e8dc1f",
+    "url": "/to-do-list/static/js/2.19e78d26.chunk.js"
+  },
+  {
+    "revision": "083dd84ec6a71d75874eb27440000914",
+    "url": "/to-do-list/static/js/2.19e78d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c46db483dd737b95af8",
+    "url": "/to-do-list/static/js/main.412209ba.chunk.js"
+  },
+  {
+    "revision": "fa27aabefbb81c21a734",
+    "url": "/to-do-list/static/js/runtime-main.80f3eba2.js"
+  }
+]);
